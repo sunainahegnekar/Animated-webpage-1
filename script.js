84da8ded4d9afd99fe2a503546b8401c276e1fff
Suneenaa <142571537+sunainahegnekar@users.noreply.github.com>
2024-04-01 22:50:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,64 @@
+gsap . from(".nav img",{
+    y:100,
+    duration:1,
+    deplay:1,
+    opacity:0,
+    stagger:0.5
+})
+
+gsap . from(".nav2  h3",{
+    y:100,
+    duration:1,
+    deplay:1,
+    opacity:0,
+       
+})
+
+gsap . from(".nav3  h3",{
+    y:100,
+    duration:1,
+    deplay:1,
+    opacity:0,
+    stagger:0.5,  
+})
+
+gsap . from(".get",{
+    y:100,
+    duration:1,
+    deplay:1,
+    opacity:0,
+    stagger:0.5,  
+})
+
+gsap . from("h1",{
+    y:100,
+    duration:1,
+    deplay:1,
+    opacity:0,
+    stagger:0.5,  
+})
+
+gsap . from("#img-left",{
+    scale:0.2,
+    opacity:0,
+})
+
+gsap . from("#img-right",{
+    scale:0.2,
+    opacity:0, 
+})
+
+gsap . from("button",{
+    y:-30,
+    repeat:-1,
+    duration:1,
+    deplay:1,
+    opacity:0,
+    stagger:0.5,
+    yoyo:true,
+
+})
+
+
+
+
